4b90df83f04afcac057fed9f3455b1a30541146d
Madflix123 <hwys172436@gmail.com>
2024-02-01 15:24:25 +0630

    Country class is created

---
+++ src/main/java/com/universal/Country.java
@@ -0,0 +1,210 @@
+package com.universal;
+
+/**
+ * Represents the world
+ */
+public class Country
+{
+    /**
+     * Country's code
+     */
+    private int Code;
+
+
+    /**
+     * Country's capital
+     */
+    private String Capital;
+
+    /**
+     * Country's code2
+     */
+    private int Code2;
+
+    /**
+     * Country's continent
+     */
+    private String Continent;
+
+
+    /**
+     * Country's GNP
+     */
+    private String GNP;
+
+    /**
+     * Country's GNPOld
+     */
+    private String GNPOld;
+
+    /**
+     * Country's governemntform
+     */
+    private String GovernmentForm;
+
+    /**
+     * Country's headofstate
+     */
+    private String HeadOfState;
+
+    /**
+     * Country's IndepYear
+     */
+    private String IndepYear;
+
+    /**
+     * Country's LifeExpectancy
+     */
+    private String LifeExpectancy;
+
+    /**
+     * Country's LocalName
+     */
+    private String LocalName;
+
+    /**
+     * Country's Name
+     */
+    private String Name;
+
+    /**
+     * Country's Population
+     */
+    private int Population;
+
+    /**
+     * Country's Region
+     */
+    private String Region;
+
+    /**
+     * Country's SurfaceArea
+     */
+    private String SurfaceArea;
+
+
+    /**
+     * Getter Setter method for all variable
+     */
+    public int getCode() {
+        return Code;
+    }
+
+    public void setCode(int code) {
+        Code = code;
+    }
+
+    public String getCapital() {
+        return Capital;
+    }
+
+    public void setCapital(String capital) {
+        Capital = capital;
+    }
+
+    public int getCode2() {
+        return Code2;
+    }
+
+    public void setCode2(int code2) {
+        Code2 = code2;
+    }
+
+    public String getContinent() {
+        return Continent;
+    }
+
+    public void setContinent(String continent) {
+        Continent = continent;
+    }
+
+    public String getGNP() {
+        return GNP;
+    }
+
+    public void setGNP(String GNP) {
+        this.GNP = GNP;
+    }
+
+    public String getGNPOld() {
+        return GNPOld;
+    }
+
+    public void setGNPOld(String GNPOld) {
+        this.GNPOld = GNPOld;
+    }
+
+    public String getGovernmentForm() {
+        return GovernmentForm;
+    }
+
+    public void setGovernmentForm(String governmentForm) {
+        GovernmentForm = governmentForm;
+    }
+
+    public String getHeadOfState() {
+        return HeadOfState;
+    }
+
+    public void setHeadOfState(String headOfState) {
+        HeadOfState = headOfState;
+    }
+
+    public String getIndepYear() {
+        return IndepYear;
+    }
+
+    public void setIndepYear(String indepYear) {
+        IndepYear = indepYear;
+    }
+
+    public String getLifeExpectancy() {
+        return LifeExpectancy;
+    }
+
+    public void setLifeExpectancy(String lifeExpectancy) {
+        LifeExpectancy = lifeExpectancy;
+    }
+
+    public String getLocalName() {
+        return LocalName;
+    }
+
+    public void setLocalName(String localName) {
+        LocalName = localName;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getPopulation() {
+        return Population;
+    }
+
+    public void setPopulation(int population) {
+        Population = population;
+    }
+
+    public String getRegion() {
+        return Region;
+    }
+
+    public void setRegion(String region) {
+        Region = region;
+    }
+
+    public String getSurfaceArea() {
+        return SurfaceArea;
+    }
+
+    public void setSurfaceArea(String surfaceArea) {
+        SurfaceArea = surfaceArea;
+    }
+}
+
+
