731cb5c66e33537b234039d5aff74fea2585b2b3
40637323 <40637323@live.napier.ac.uk>
2024-02-08 22:35:24 +0630

    integration testing for report methods

--- src/test/java/com/universal/AppIntegrationTest.java
+++ src/test/java/com/universal/AppIntegrationTest.java
@@ -22,10 +22,88 @@ public class AppIntegrationTest
     }
 
     @Test
+    public void testGetCountries() {
+        List<Country> countries = app.getCountries();
+        assertNotNull(countries);
+        System.out.println("Get Country Function's output is not null");
+    }
+
+    @Test
     public void testGetCountriesInAfrica() {
         List<Country> africanCountries = app.getCountriesInAfrica();
         assertNotNull(africanCountries);
         System.out.println("Get Country In Africa Function's output is not null");
     }
+
+    @Test
+    public void testGetCountriesInCentralAfrica() {
+        List<Country> centralAfricanCountries = app.getCountriesInCentralAfrica();
+        assertNotNull(centralAfricanCountries);
+        System.out.println("Get Country In Central Africa Function's output is not null");
+    }
+//
+//    @Test
+//    void displayisNull() {
+//        List<Country> countries = new ArrayList<>();
+//        app.displayCountriesOrderedByPopulation(countries);
+//    }
+
+    @Test
+    public void testGetCitiesOrderByPopulation() {
+        List<City> cityWorld = app.getCitiesOrderedByPopulation();
+        assertNotNull(cityWorld);
+        System.out.println("Get Cities ordered by population Function's output is not null");
+    }
+
+    @Test
+    public void testGetCitiesByContinentOrderByPopulation() {
+        List<City> cityContinent = app.getCitiesByContinentOrderedByPopulation();
+        assertNotNull(cityContinent);
+        System.out.println("Get Cities by Continent Function's output is not null");
+
+    }
+
+    @Test
+    public void testGetCitiesByRegionOrderByPopulation() {
+        List<City> cityRegion = app.getCitiesByRegionOrderedByPopulation();
+        assertNotNull(cityRegion);
+        System.out.println("Get Cities by Region Function's output is not null");
+    }
+
+    @Test
+    public void testGetCitiesInCountryOrderByPopulation() {
+        List<City> cityCountry = app.getCitiesInCountryOrderedByPopulation();
+        assertNotNull(cityCountry);
+        System.out.println("Get Cities in Country Function's output is not null");
+    }
+
+    @Test
+    public void testGetCitiesInDistinct() {
+        List<City> cityDiscrict = app.getCitiesInDistrict();
+        assertNotNull(cityDiscrict);
+        System.out.println("Get Cities in Distinct Function's output is not null");
+    }
+
+    @Test
+    public void testGetAllCapitalCitiesByPopulation() {
+        List<City> CapitalCities = app.getAllCapitalCitiesByPopulation();
+        assertNotNull(CapitalCities);
+        System.out.println("Get all Capital Cities by Population Function's output is not null");
+    }
+
+    @Test
+    public void testCapitalInContinent() {
+        List<City> CapitalContinent = app.getCapitalCitiesByContinentOrderedByPopulation();
+        assertNotNull(CapitalContinent);
+        System.out.println("Get Capital Cities by Continent Function's output is not null");
+    }
+
+    @Test
+    public void testCapitalInRegion() {
+        List<City> CapitalRegion = app.getCapitalCitiesByRegionOrderedByPopulation();
+        assertNotNull(CapitalRegion);
+        System.out.println("Get Capital Cities by Region Function's output is not null");
+    }
+
 
 }
