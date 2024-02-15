4c3cb77dbf5f5f02350d4ae0cc6fa90cb829d039
PaPaMyintAung03 <40637324@live.napier.ac.uk>
2024-02-15 14:17:18 +0630

    Retrieving top 7 countries from the SouthEast Asia region

--- src/main/java/com/universal/App.java
+++ src/main/java/com/universal/App.java
@@ -109,7 +109,7 @@ public class App
         }
         try (Statement stmt = con.createStatement()) {
             String sql = "SELECT country.code, country.name, country.continent, country.region, country.population, city.name\n" +
-                    "FROM cit\n" +
+                    "FROM city\n" +
                     "INNER JOIN country\n" +
                     "ON city.id = country.capital\n" +
                     "WHERE country.continent = 'Asia'\n" +
@@ -134,6 +134,48 @@ public class App
         }
         return topCountriesContinent;
     }
+
+    /**
+     * Retrieves a list top 7 countries from the SouthEast Asia region and orders them by population.
+     * @return a List of Country for representing countries in Central Africa.
+     */
+    public List<Country> getTopSevenCountriesInSouthEastAsia() {
+        List<Country> topSevenCountriesInRegion = new ArrayList<>();
+        if (con == null)
+        {
+            System.out.println("No connection");
+            return topSevenCountriesInRegion;
+        }
+        try (Statement stmt = con.createStatement()) {
+            String sql = "SELECT country.code, country.name, country.continent, country.region, country.population, city.name\n" +
+                    "FROM cit\n" +
+                    "INNER JOIN country\n" +
+                    "ON city.id = country.capital\n" +
+                    "WHERE country.continent = 'Asia' AND country.region = 'Southeast Asia'\n" +
+                    "ORDER BY country.population DESC\n" +
+                    "LIMIT 7";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                Country country = new Country();
+                country.setCode(rs.getString("country.code"));
+                country.setName(rs.getString("country.name"));
+                country.setContinent(rs.getString("country.continent"));
+                country.setRegion(rs.getString("country.region"));
+                country.setPopulation(rs.getLong("country.population"));
+                country.setCapital(rs.getString("city.name"));
+                // Set other attributes as necessary
+
+                topSevenCountriesInRegion.add(country);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error executing query: " + e.getMessage());
+        }
+        return topSevenCountriesInRegion;
+    }
+
+
+
     public void displayCountries(List<Country> list) {
         if (con == null) {
             System.out.println("No connection");
