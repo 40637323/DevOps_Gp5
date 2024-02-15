54acc7d018573d24072c9b4fcb851e927fbf2bcc
Hein Htet Naing <135005782+heinhn@users.noreply.github.com>
2024-02-16 01:05:48 +0630

    The population of the world.

--- src/main/java/com/universal/App.java
+++ src/main/java/com/universal/App.java
@@ -589,6 +589,35 @@ public class App
             System.out.println("Error executing query: " + e.getMessage());
         }
     }
+    //Display World Population
+    public void printWorldPopulation() {
+        if (con == null) {
+            System.out.println("No connection");
+            return;
+        }
+        String sql = "SELECT SUM(Population) AS WorldPopulation FROM country;";
+
+        try (Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            System.out.println("\u001B[1mWorld Population Report\u001B[0m");
+            System.out.println("+------------------+");
+            System.out.println("| World Population |");
+            System.out.println("+------------------+");
+
+            if (rs.next()) {
+                long worldPopulation = rs.getLong("WorldPopulation");
+                System.out.printf("| %,16d |\n", worldPopulation);
+            }
+
+            System.out.println("+------------------+");
+        } catch (SQLException e) {
+            System.out.println("Error executing query: " + e.getMessage());
+        }
+    }
+
+
+
 
     //Display the select Language
     public void printSelectedLanguageSpeakers() {
@@ -713,6 +742,7 @@ public class App
         a.printRegionPopulationReport();
         a.printCountryPopulationReport();
         a.printSelectedLanguageSpeakers();
+        a.printWorldPopulation();
 
 
         // Disconnect from database
